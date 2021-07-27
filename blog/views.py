eda7d1170dc007cbb6267e6145fe0cfd877cbb5c
Patrick O'Rourke <Patrick.ORourke160893@gmail.com>
2021-07-27 19:45:22 +0100

    Added code from Video 5 of YouTube Tutorial

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
 # This function will haandle the traffic from the home page of the Blog.
 
 
-posts = [
-    {
-        'author': 'Patrick O Rourke',
-        'title' : 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': 'July 10, 2021'
-
-    },
-{
-        'author': 'Britany Spears',
-        'title' : 'Blog Post 2',
-        'content': 'Second Post Content',
-        'date_posted': 'July 11, 2021'
-
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request,'blog/home.html', context)
 
